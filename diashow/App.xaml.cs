63e4f60932653e7402e8a3a0ae36dabcedf49b52
ttomczek <>
2026-09-07 19:32:33 +0200

    Empty project

---
+++ diashow/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace diashow;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
